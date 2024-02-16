0fc34abe097efa5a5b95824e5a883546b8e2ed5d
omegacms <omegacms@outlook.com>
2024-02-16 16:43:46 +0100

    Adding phpdoc block

--- database/migrations/002_ChangeQuantity.php
+++ database/migrations/002_ChangeQuantity.php
@@ -1,13 +1,41 @@
 <?php
+/**
+ * Omega Application.
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
 
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @use
+ */
 use Omega\Database\Adapter\AbstractDatabaseAdapter;
 
+/**
+ * Change quantity class.
+ * 
+ * @category    Application
+ * @package     Application\Database
+ * @subpackage  Application\Database\Migration
+ * @link        https://omegacms.github.io
+ * @author      Adriano Giovannini <omegacms@outlook.com>
+ * @copyright   Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license     https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ * @version     1.0.0
+ */
 class ChangeQuantity
 {
     /**
      * Alter tables orders.
      *
-     * @param  AbstractDatabaseAdapter $connection
+     * @param  AbstractDatabaseAdapter $connection Holds the current connection instance.
      * @return void
      */
     public function migrate( AbstractDatabaseAdapter $connection ) : void
