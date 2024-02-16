6af61831cee6ac6b135ae748b2dd5a1dd47a4ec4
omegacms <omegacms@outlook.com>
2024-02-16 17:33:03 +0100

    Update the App\Models doc block

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Part of Banco Alimentare Toscana - App\Models Package
+ * Part of Omega CMS - App/Models Package
  *
- * @link       https://github.com/adrix71/banco-alimentare
- * @author     Adriano Giovannini <dev@agmedia.io>
- * @copyright  Copyright (c) 2022 Banco Alimentare Toscana. (https://www.bancoalimentare.it/it/toscana)
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
  * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
  */
 
