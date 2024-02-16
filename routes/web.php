f8fc052188772217af07a236cb33d749ba597273
omegacms <omegacms@outlook.com>
2024-02-16 16:46:57 +0100

    Adding phpdoc block

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,21 @@
 <?php
+/**
+ * Omega Application - config/cache configuration file.
+ *
+ * @link       https://omegacms.github.io
+ * @author     Adriano Giovannini <omegacms@outlook.com>
+ * @copyright  Copyright (c) 2022 Adriano Giovannini. (https://omegacms.github.io)
+ * @license    https://www.gnu.org/licenses/gpl-3.0-standalone.html     GPL V3.0+
+ */
 
+/**
+ * @declare
+ */
+declare( strict_types = 1 );
+
+/**
+ * @use
+ */
 use App\Http\Controllers\ShowHomePageController;
 use App\Http\Controllers\Products\OrderProductController;
 use App\Http\Controllers\Products\OrderConfirmationController;
@@ -10,7 +26,10 @@ use App\Http\Controllers\Users\RegisterUserController;
 use App\Http\Controllers\Users\ShowRegisterFormController;
 use Omega\Routing\Router;
 
-return function(Router $router) {
+/**
+ * Return an array of route path.
+ */
+return function( Router $router ) {
     $router->errorHandler(
         404, fn() => 'whoops!'
     );
