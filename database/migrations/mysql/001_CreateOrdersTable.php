734de7e5b9536ac65b4149ba135775449b91411b
omegacms <omegacms@outlook.com>
2024-05-01 16:10:52 +0200

    Update orders table

--- database/migrations/mysql/001_CreateOrdersTable.php
+++ database/migrations/mysql/001_CreateOrdersTable.php
@@ -45,9 +45,10 @@ class CreateOrdersTable
         $table->int( 'user_id' );
         $table->int( 'product_id' );
         $table->int( 'quantity' )->default( 1 );
-        $table->numeric( 'total_price' )->nullable();
+        $table->int( 'total_price' )->nullable();
         $table->bool( 'is_confirmed' )->default( false );
         $table->dateTime( 'ordered_at' )->default( 'CURRENT_TIMESTAMP' );
+		$table->dateTime( 'completed_at' )->nullable();
         $table->text( 'notes' );
         $table->text( 'delivery_instruction' );
         $table->execute();
