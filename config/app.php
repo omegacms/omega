9f7acbeb06f5918823b4d019ae927e9e4b6afc97
omegacms <omegacms@outlook.com>
2024-09-23 21:57:01 +0200

    Adding facade pattern and refactor migrations. Warning, fix not tested

--- config/app.php
+++ config/app.php
@@ -14,6 +14,29 @@
 declare( strict_types = 1 );
 
 /**
+ * @use
+ */
+use Omega\Config\ServiceProvider\ConfigServiceProvider;
+use Omega\Cache\ServiceProvider\CacheServiceProvider;
+use Omega\Database\ServiceProvider\DatabaseServiceProvider;
+use Omega\Queue\ServiceProvider\QueueServiceProvider;
+use Omega\Http\ServiceProvider\ResponseServiceProvider;
+use Omega\Session\ServiceProvider\SessionServiceProvider;
+use Omega\Validation\ServiceProvider\ValidationServiceProvider;
+use Omega\View\ServiceProvider\ViewServiceProvider;
+use Omega\Support\Facades\Config;
+use Omega\Support\Facades\Cache;
+use Omega\Support\Facades\Database;
+use Omega\Support\Facades\Email;
+use Omega\Support\Facades\Filesystem;
+use Omega\Support\Facades\Logging;
+use Omega\Support\Facades\Queue;
+use Omega\Support\Facades\Response;
+use Omega\Support\Facades\Session;
+use Omega\Support\Facades\Validation;
+use Omega\Support\Facades\View;
+
+/**
  * Return an array with common application parameters.
  */
 return [
@@ -25,4 +48,38 @@ return [
 	 * geographical location of the application.
 	 */
 	'timezone' => 'Europe/Rome',
+
+	/**
+	 * Array of ServiceProviders.
+	 */
+	'providers' => [
+        ConfigServiceProvider::class,
+        CacheServiceProvider::class,
+        DatabaseServiceProvider::class,
+        //EmailServiceProvider::class,
+        //FilesystemServiceProvider::class,
+        //LoggingServiceProvider::class,
+        QueueServiceProvider::class,
+        ResponseServiceProvider::class,
+        SessionServiceProvider::class,
+        ValidationServiceProvider::class,
+        ViewServiceProvider::class,
+    ],
+
+	/**
+	 * Array of facades class.
+	 */
+	'facades'   => [
+        'Config'     => Config::class,
+        'Cache'      => Cache::class,
+        'Database'   => Database::class,
+        //'Email'      => Email::class,
+        //'Filesystem' => Filesystem::class,
+        //'Logging'    => Logging::class,
+        'Queue'      => Queue::class,
+        'Response'   => Response::class,
+        'Session'    => Session::class,
+        'Validation' => Validation::class,
+        'View'       => View::class
+    ],
 ];
--- database/migrations/schemes/002_CreateProductsTable.php
+++ database/migrations/schemes/002_CreateProductsTable.php
@@ -47,6 +47,31 @@ class CreateProductsTable
         $table->text( 'description' );
         $table->int( 'price' )->nullable();
         $table->execute();
+
+        $products = [
+            [
+                'name'        => 'Space Tour',
+                'description' => 'Take a trip on a rocket ship. Our tours are out of this world. Sign up now for a journey you won&apos;t soon forget.',
+                'price'       => 12.50
+            ],
+            [
+                'name'        => 'Large Rocket',
+                'description' => 'Need to bring some extra space-baggage? Everyone asking you to bring back a moon rock for them? This is the rocket you want...',
+                'price'       => 25.30
+            ],
+            [
+                'name'        => 'Small Rocket',
+                'description' => 'Space exploration is expensive. This rocket comes in under budget and atmosphere.',
+                'price'       => 50
+            ],
+        ];
+
+        foreach ( $products as $product ) {
+            $connection
+                ->query()
+                ->from( $this->table )
+                ->insert( [ 'name', 'description', 'price' ], $product );
+        }
     }
 
     /**
